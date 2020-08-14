4e8c48827f955348548a90e3fdce7b4c1a636411
Roger Chao <chaoshuangjui@gmail.com>
2020-08-14 13:35:27 +0800

    send mail example

---
+++ ConsoleSendMail/ConsoleSendMail/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace ConsoleSendMail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mail_send();
+            Console.WriteLine("Mail Sent Successfully!");
+            Console.ReadLine();
+        }
+
+        private static void mail_send()
+        {
+            var smtpClient = new SmtpClient("host")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("username", "password"),
+                EnableSsl = false,
+            };
+
+            smtpClient.Send("email", "recipient", "subject", "body");
+        }
+    }
+}
