9a009c9d361efc7767a5c334fe1ff9c9338984a6
Roger Chao <chaoshuangjui@gmail.com>
2020-08-14 16:50:43 +0800

    staging area, send mail, ok

--- ConsoleSendMail/ConsoleSendMail/Program.cs
+++ ConsoleSendMail/ConsoleSendMail/Program.cs
@@ -15,14 +15,15 @@ namespace ConsoleSendMail
 
         private static void mail_send()
         {
-            var smtpClient = new SmtpClient("host")
+            var smtpClient = new SmtpClient("csmail2.tradevan.com.tw")
             {
-                Port = 587,
-                Credentials = new NetworkCredential("username", "password"),
+                Port = 25,
+                Credentials = new NetworkCredential("", ""),
                 EnableSsl = false,
             };
 
-            smtpClient.Send("email", "recipient", "subject", "body");
+            //smtpClient.Send("rogerchao@172.20.23.244", "chaoshuangjui@gmail.com", "Test主題", "12345678");
+            smtpClient.Send("service.utm@tradevan.com.tw", "chaoshuangjui@gmail.com", "Test從驗證區寄信", "驗證區寄到我的信箱");
         }
     }
 }
